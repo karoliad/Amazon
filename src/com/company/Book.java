ab106603974152445c92d5e79f53dd897d180e44
karoliad <karoline.devold@gmail.com>
2020-01-18 14:10:37 +0100

    added arrayList and created findmoviebyId()

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,63 +1,59 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Book {
-//Instance-variabler
+    //Instance-variabler
     private String title;
     private String author;
-    private  int price;
+    private int price;
 
-//  Opprett arraylist for lagring av bøker
-    static ArrayList<Book> bookList = new ArrayList<>(); // nono this must go! flytt til main
-
-
-//   Constructor for opprettelse av objektet bok
+    //   Constructor for opprettelse av objektet bok
     public Book(String title, String author, int price) {
         this.title = title;
         this.author = author;
         this.price = price;
     }
 
-//Getters og setters for bøker
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public int getPrice() { return price; }
-    public void setTitle(String title) { this.title = title; }
-    public void setAuthor(String author) { this.author = author; }
-    public void setPrice(int price) { this.price = price; }
-
-//Opprett nytt bok-objekt og legger til i arrayList, krever de tre parameterne som er definert i constructoren
-    public Book newBook(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter book title");
-        String title = scanner.nextLine();
-        System.out.println("Please Author");
-        String author = scanner.nextLine();
-        System.out.println("Please enter price");
-        int price = scanner.nextInt();
-        Book myNewBook = new Book(title, author, price);
-        bookList.add(myNewBook);
-
-        return myNewBook;
+    @Override
+    public String toString() {
+        return "Book" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                "\n";
     }
 
-// Skriv ut kvittering for bok med tittel, forfatter og pris
-    public void printBook() {
-        System.out.println("\n- MY FAVOURITE BOOK IS -\nTitle: " + title +
-                "\nAuthor: " + author + "\nPrice: " + price+"\n");
+    // Skriv ut kvittering for bok med tittel, forfatter og pris
+    public void printBookDetails() {
+        System.out.println("\nTitle: " + title +
+                "\nAuthor: " + author + "\nPrice: " + price);
     }
 
-////    Oppretter array for å lagre bøkene som er skrevet inn
-//    static void opprettBookArray(Book book, Book book1) {
-//        Book[] myBooks = new Book[2];
-//        myBooks[0] = book;
-//        myBooks[1] = book1;
-//
-//        for (int i = 0; i < myBooks.length; i++) {
-//            Book.printBook(myBooks[i]);
-//        }
+    //Getters og setters for bøker
+    public String getTitle() {
+        return title;
     }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,55 @@
 package com.company;
-
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 
 
-// TEST fredag 17.01.20
-
+// TEST1 branch fra github  fredag 17.01.20
 
 public class Main {
+
     public static void main(String[] args) {
+       List<Book> arrayListbooks = new ArrayList<>();
+        arrayListbooks.add(new Book("Lord of the Rings", "Tolkien", 399));
+        arrayListbooks.add(new Book("Harry Potter", "Rowling", 199));
+        arrayListbooks.add(new Book("Hypnotisøren", "Kepler", 99));
+        arrayListbooks.add(new Book("Spøkelse", "Jo Nesbø", 159));
+        arrayListbooks.add(new Book("Snarveien til vin  ", "Ingvild Tennfjord", 379));
+        arrayListbooks.add(new Book("Bobler ", "Ingvild Tennfjord", 99));
 
-// PRINTER UT BØKER
-//    Book nyBok = Book.newBook();
-//    Book.printBook(kvittering);
-//        System.out.println("skriver ut booklist: "+ Book.bookList.indexOf(0) );
+        List<Movie> arrayListMovies = new ArrayList<>();
+        arrayListMovies.add(new Movie("Lord of the Rings", Movie.MovieGenre.ACTION, 399,"Peter Jackson", 1001));
+        arrayListMovies.add(new Movie("Harry Potter", Movie.MovieGenre.DOCUMENTARY, 199,1002));
+        arrayListMovies.add(new Movie("Hypnotisøren", Movie.MovieGenre.DRAMA, 99, "Julenissen", 1003));
+        arrayListMovies.add(new Movie("Spøkelse", Movie.MovieGenre.ACTION, 299,1004));
 
+        findMovieById(arrayListMovies, 1004);
+        findMovieById(arrayListMovies, 1002);
 
-// PRINTER UT FILMER
-    Movie kvittering= Movie.newMovie();
-    Movie.printMovieDetails(kvittering);
+//        for (Book book : arrayListbooks) {
+//         book.printBookDetails();
+//        }
+//        System.out.println("\n________Movies_________");
+//
+//        for (Movie movie:arrayListMovies) {
+//         movie.printMovieDetails();
+//        }
+
     }
+
+     public static void findMovieById(List<Movie> arrayListMovies, long productId){
+         boolean noMoviesFound = true;
+         for (Movie movie : arrayListMovies) {
+             boolean foundMovie = movie.getProductId() == productId;
+             if(foundMovie) {
+                 System.out.println("Found this movie: " +"ProductId "+ productId);
+                 movie.printMovieDetails();
+                 noMoviesFound = false;
+             }
+         }
+         if(noMoviesFound){
+             System.out.println("Didn`t find a movie with productId " + productId);
+         }
+     }
+
+
 }
